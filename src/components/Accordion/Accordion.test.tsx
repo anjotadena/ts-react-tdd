799b7e5112dc18afe68b3212002add7017048c56
Anjo Tadena <tadena.anjo@gmail.com>
2022-04-03 23:55:42 +0800

    Create accordion component

---
+++ src/components/Accordion/Accordion.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Button from './Accordion';
+
+describe('renders button', () => {
+  it('xx', () => {
+    expect(true).toBeTruthy();
+  });
+});
