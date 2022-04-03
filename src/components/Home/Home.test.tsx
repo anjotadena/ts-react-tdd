839d0236fed49f4629888a4a1ac28dee92ca1669
Anjo Tadena <tadena.anjo@gmail.com>
2022-04-04 06:19:38 +0800

    Update Home test

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -11,15 +11,15 @@ jest.mock('axios');
 
 let container: HTMLElement;
 
-beforeEach(async () => {
-  await act(async () => {
-    container = render(<Home />).container;
-  });
-});
+beforeEach(async () => {});
 
 afterEach(cleanup);
 
-it('should show homes', () => {
+it('should show homes', async () => {
+  await act(() => {
+    container = render(<Home />).container;
+  });
+
   const homes = getAllByTestId(container, 'home');
 
   expect(homes.length).toBeGreaterThan(0);
